dfe04d5ad803757f4d96bea5683cd93bc19f061f
Alessandro Sordoni <alsordon@microsoft.com>
2017-07-06 14:33:43 +0000

    simple sampler

--- sample_imdb_simple.py
+++ sample_imdb_simple.py
@@ -11,12 +11,15 @@ from ipdb import set_trace as dbg
 
 import theano
 import theano.tensor as T
+from lm_data import IMDB_JMARS
 from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
+from lm_lstm_imdb import (init_params, init_tparams, load_params,
+        is_train, build_rev_model, build_gen_model,
+        build_sampler, gen_sample, beam_sample)
 
 
 def build_parser():
     parser = argparse.ArgumentParser()
-
     parser.add_argument("model_prefix", help="Model path")
     parser.add_argument("--seqlen", type=int, default=50,
                         help="Sequence length. Default: %(default)s")
@@ -27,7 +30,6 @@ def build_parser():
     parser.add_argument("--show-real-data", action="store_true",
                         help="Show real data from validset instead sampling.")
     parser.add_argument("-v", "--verbose", action="store_true", help="Verbose mode.")
-
     return parser
 
 
@@ -37,24 +39,20 @@ def main():
 
     np.random.seed(args.seed)
     trng = RandomStreams(args.seed)
-    rng = np.random.RandomState(args.seed+1)
+    rng = np.random.RandomState(args.seed + 1)
     model_file = args.model_prefix + "_pars.npz"
     model_opts = args.model_prefix + "_opts.pkl"
     model_options = pkl.load(open(model_opts, 'rb'))
 
     # Load data
-    from lm_data import IMDB_JMARS
-    data = IMDB_JMARS("./experiments/data", seq_len=16, batch_size=args.nb_samples, topk=16000)
+    data = IMDB_JMARS("./experiments/data", seq_len=16,
+                      batch_size=args.nb_samples, topk=16000)
     model_options["dim_input"] = data.voc_size
 
-    if args.show_real_data:
-        for x, y, x_mask in data.get_valid_batch():
-            data.print_batch(x)
-            raw_input("-- More --")
-        sys.exit(0)
+    for num, (x, y, x_mask) in enumerate(data.get_valid_batch()):
+        data.print_batch(x)
+        break
 
-    from lm_lstm_imdb import init_params, init_tparams, load_params
-    from lm_lstm_imdb import is_train, build_rev_model, build_gen_model, build_sampler, gen_sample, beam_sample
     params = init_params(model_options)
     print('Loading model parameters...')
     params = load_params(model_file, params)
@@ -70,7 +68,8 @@ def main():
     is_train.tag.test_value = np.float32(0.)
 
     zmuv = T.tensor3('zmuv')
-    zmuv.tag.test_value = np.ones((11, 20, model_options['dim_z'])).astype("float32")
+    zmuv.tag.test_value = np.ones((
+        11, 20, model_options['dim_z'])).astype("float32")
 
     # build the symbolic computational graph
     nll_rev, states_rev, updates_rev = \
@@ -82,16 +81,14 @@ def main():
     f_next = build_sampler(tparams, model_options, trng, provide_z=True)
     # Build inference
     get_latents = theano.function([x, y, x_mask, zmuv], z,
-        updates=(updates_gen + updates_rev),
-        givens={is_train: np.float32(0.)})
-
-    indices = np.arange(len(data.va_words))
+                                  updates=(updates_gen + updates_rev),
+                                  givens={is_train: np.float32(0.)})
     while True:
         s1 = raw_input("s1:").strip().split()
         s2 = raw_input("s2:").strip().split()
 
-        s1_id = [data.word2idx.get(x, data.unk_id) for x in s1]
-        s2_id = [data.word2idx.get(x, data.unk_id) for x in s2]
+        s1_id = [data.word2idx.get(word, data.unk_id) for word in s1]
+        s2_id = [data.word2idx.get(word, data.unk_id) for word in s2]
 
         batch = data.prepare_batch([s1_id, s2_id])
         data.print_batch(batch[0])
@@ -108,7 +105,8 @@ def main():
             print("{}: ".format(i), end="")
             z = ((1 - i) * z1) + (i * z2)  # Interpolate latent
             z = np.repeat(z, 10, axis=1)
-            sample, sample_score = beam_sample(tparams, f_next, model_options,
+            sample, sample_score = beam_sample(
+                tparams, f_next, model_options,
                 maxlen=20, zmuv=z, unk_id=data.unk_id,
                 eos_id=data.eos_id, bos_id=data.bos_id)
             sample = [sample[0]]
@@ -123,9 +121,10 @@ def main():
             print("{}: ".format(i), end="")
             z = ((1 - i) * z1) + (i * z2)  # Interpolate latent
             z = np.repeat(z, 10, axis=1)
-            sample, sample_score = gen_sample(tparams, f_next, model_options,
-                    maxlen=20, argmax=False, zmuv=z,
-                    unk_id=data.unk_id, eos_id=data.eos_id, bos_id=data.bos_id)
+            sample, sample_score = gen_sample(
+                tparams, f_next, model_options,
+                maxlen=20, argmax=False, zmuv=z,
+                unk_id=data.unk_id, eos_id=data.eos_id, bos_id=data.bos_id)
             sample = [sample.T[np.argsort(sample_score)[-1]]]
             data.print_batch(sample, eos_id=data.eos_id, print_number=False)
 
@@ -136,7 +135,8 @@ def main():
         for i in np.linspace(0, 1, 11):
             print("{}: ".format(i), end="")
             z = ((1 - i) * z1) + (i * z2)  # Interpolate latent
-            sample, sample_score = gen_sample(tparams, f_next, model_options,
+            sample, sample_score = gen_sample(
+                tparams, f_next, model_options,
                 maxlen=20, argmax=True, zmuv=z,
                 unk_id=data.unk_id, eos_id=data.eos_id, bos_id=data.bos_id)
             data.print_batch(sample.T, eos_id=data.eos_id, print_number=False)
@@ -147,4 +147,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
