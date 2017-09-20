299cfdeb786c515a1c71a6fc704542996a68d188
Alessandro Sordoni <alsordon@microsoft.com>
2017-09-20 19:51:04 +0000

    import fuel

--- util.py
+++ util.py
@@ -7,6 +7,10 @@ import six
 from theano.ifelse import ifelse
 from collections import OrderedDict
 import warnings
+import fuel
+from fuel.datasets import SVHN
+from fuel.schemes import ShuffledScheme
+from fuel.streams import DataStream
 
 logit = lambda u: T.log(u / (1.-u))
 logit_np = lambda u: np.log(u / (1.-u)).astype(theano.config.floatX)
@@ -237,9 +241,6 @@ def unzip(zipped):
         new_params[kk] = vv.get_value()
     return new_params
 
-from fuel.datasets import SVHN
-from fuel.schemes import ShuffledScheme
-from fuel.streams import DataStream
 
 def create_streams(train_set, valid_set, test_set, training_batch_size,
                    monitoring_batch_size):
@@ -283,54 +284,6 @@ def create_streams(train_set, valid_set, test_set, training_batch_size,
     return (main_loop_stream, train_monitor_stream, valid_monitor_stream,
             test_monitor_stream)
 
-
-def create_svhn_streams(training_batch_size, monitoring_batch_size):
-    """Creates SVHN data streams.
-    Parameters
-    ----------
-    training_batch_size : int
-        Batch size for training.
-    monitoring_batch_size : int
-        Batch size for monitoring.
-    Returns
-    -------
-    rval : tuple of data streams
-        Data streams for the main loop, the training set monitor,
-        the validation set monitor and the test set monitor.
-    """
-    train_set = SVHN(2, ('train',), sources=('features',),
-                     subset=slice(0, 63257))
-    valid_set = SVHN(2, ('train',), sources=('features',),
-                     subset=slice(63257, 73257))
-    test_set = SVHN(2, ('test',), sources=('features',))
-
-    return create_streams(train_set, valid_set, test_set, training_batch_size,
-                          monitoring_batch_size)
-
-def create_gaussian_mixture_data_streams(batch_size, monitoring_batch_size,
-                                         means=None, variances=None, priors=None,
-                                         rng=None, num_examples=100000,
-                                         sources=('features', )):
-    train_set = GaussianMixture(num_examples=num_examples, means=means,
-                                variances=variances, priors=priors,
-                                rng=rng, sources=sources)
-
-    valid_set = GaussianMixture(num_examples=num_examples,
-                                means=means, variances=variances,
-                                priors=priors, rng=rng, sources=sources)
-
-    main_loop_stream = DataStream(
-        train_set,
-        iteration_scheme=ShuffledScheme(
-            train_set.num_examples, batch_size=batch_size, rng=rng))
-
-    train_monitor_stream = DataStream(
-        train_set, iteration_scheme=ShuffledScheme(5000, batch_size, rng=rng))
-
-    valid_monitor_stream = DataStream(
-        valid_set, iteration_scheme=ShuffledScheme(5000, batch_size, rng=rng))
-
-    return main_loop_stream, train_monitor_stream, valid_monitor_stream
 
 def segment_axis(a, length, overlap=0, axis=None, end='cut', endvalue=0):
     """Generate a new array that chops the given array along the given axis
