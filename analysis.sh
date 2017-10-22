577d8f15fab892aa8d8218941b7d8e8ddc06898c
Amjad Almahairi <amjadmahayri@gmail.com>
2017-10-22 17:34:30 +0000

    script improvment

--- analysis.sh
+++ analysis.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
 for file in `ls $1/*_log.txt`; do
+  niter=`cat $file|grep Valid|wc -l` 
   perf=`cat $file|grep Valid|tail -n 1`
   file=`basename $file`
-  echo "$file --> $perf"
+  printf "%-100s --> (%s): %s" "$file" "$niter" "$perf"
+  echo
 done
