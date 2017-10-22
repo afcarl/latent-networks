6887b4dc9eefdcf4d882c84b4f9ba1240bca7ed0
alsordon <alsordon@microsoft.com>
2017-10-22 12:32:47 -0700

    specify epoch

--- analysis.sh
+++ analysis.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-for file in `ls $1/*_log.txt`; do
-  niter=`cat $file|grep Valid|wc -l` 
-  perf=`cat $file|grep Valid|tail -n 1`
+if [ "$#" -eq 2 ]; then
+  niter=$2
+else
+  niter=100
+fi
+
+for file in `ls $1/*txt`; do
+  niter=`cat $file|grep Valid|head -n $niter|wc -l` 
+  perf=`cat $file|grep Valid|head -n $niter|tail -n 1`
   file=`basename $file`
   printf "%-100s --> (%s): %s" "$file" "$niter" "$perf"
   echo
